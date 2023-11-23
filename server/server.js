340afece7c8ef6faa35ec54c64f93b32ed700e78
ryan210757 <kryanharris1@gmail.com>
2023-11-23 15:17:01 -0600

    fix secret leak

--- server/server.js
+++ server/server.js
@@ -1,20 +1,22 @@
 const express = require('express')
 const cors = require('cors')
+const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const postRoutes= require('./routes/posts.js')
 
 const app = express()
+dotenv.config()
 
 app.use(bodyParser.json({limit: "30mb", extended: true}))   //important middleware for form's input
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
 app.use(cors())
 app.use('/posts', postRoutes)
 
-const uri = 'mongodb+srv://cheesecakes:yhYE5T9T8nne8ZAP@cheesecakes.frbv9ae.mongodb.net/?retryWrites=true&w=majority'
+
 const PORT = 4000 || process.env.PORT
 
-mongoose.connect(uri)
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => {
         app.listen(PORT, () => console.log("server listening at port 4000"))
     })
