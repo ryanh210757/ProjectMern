e4d8ecc0179f63a2b36c817b42f51d4e45c143dc
ryanh210757 <kryanharris1@gmail.com>
2023-11-30 22:59:37 -0600

    install tailwind

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,28 +1,43 @@
-import {AppBar, Toolbar, Typography, Grid, Tabs, Tab} from '@material-ui/core'
-import React, {useState} from 'react'
-import useStyles from '../styles'
-import DrawerComp from './Drawer'
-export default function Navbar({links}){
-    const classes = useStyles()
-    const [value, setValue] = useState(0)
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
-    return (
-        <AppBar>
-            <Toolbar>
-               <Grid container>
-               <Typography variant="h4" align="center">Cheesecakes Galore & More</Typography>
-                    <Grid item xs={2}>
-                    </Grid>
-                    <Grid item xs={5}>
-                        <Tabs indicatorcolor="secondary" textcolor="inherit" value={value} onChange={(e, val) => setValue(val)}>
-                           {links.map((link, index) => (
-                            <Tab key={index} label={link}/>
-                           ))}
-                        </Tabs>
-                    </Grid>
-               </Grid>
-               <DrawerComp />
-            </Toolbar>
-        </AppBar>
-    )
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Cheesecakes Galore & More
+          </Typography>
+          <Button color="inherit">Home</Button>
+          <Button color="inherit">Menu</Button>
+          <Button color="inherit">About</Button>
+          <Button color="inherit">Contact</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
